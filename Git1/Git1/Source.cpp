6d755551c9bef66785bc407ffb4bf13c14bc8fd6
RonaldasAnt <ronaldas2002@gmail.com>
2019-01-20 11:17:06 +0200

    asfdsafs

--- Git1/Git1/Source.cpp
+++ Git1/Git1/Source.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-
+#include <fstream>
+#include <string>
 using namespace std;
 
-int main()
-{
-	cout << "Hello World" << endl;
+int main() {
+	string base(".txt");
+	for (int i = 1; i <= 5; ++i) {
+		ofstream(to_string(i) + base);
+	}
 	return 0;
 }
